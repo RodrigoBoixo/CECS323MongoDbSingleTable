5e8f3cc9354a8a89dfdf2b1feb4723809b65daec
Rodrigo Boixo <rboixo@gmail.com>
2023-10-24 21:58:36 -0700

    added a department file that has the department methods, you actually add the department and connect to the database in main

---
+++ Department.py
@@ -0,0 +1,67 @@
+from pymongo import MongoClient
+
+def validate_department(department):
+  """Validates a department document."""
+
+  # Check if the required fields are present.
+  if not department.get("abbreviation"):
+    raise ValueError("The department abbreviation is required.")
+  if not department.get("chair_name"):
+    raise ValueError("The department chair name is required.")
+  if not department.get("building"):
+    raise ValueError("The department building is required.")
+  if not department.get("office"):
+    raise ValueError("The department office is required.")
+  if not department.get("description"):
+    raise ValueError("The department description is required.")
+
+  # Check the maximum lengths of the fields.
+  if len(department["name"]) > 50:
+    raise ValueError("The department name must be no longer than 50 characters.")
+  if len(department["abbreviation"]) > 6:
+    raise ValueError("The department abbreviation must be no longer than 6 characters.")
+  if len(department["chair_name"]) > 80:
+    raise ValueError("The department chair name must be no longer than 80 characters.")
+  if len(department["building"]) > 10:
+    raise ValueError("The department building must be no longer than 10 characters.")
+  if len(department["description"]) > 80:
+    raise ValueError("The department description must be no longer than 80 characters.")
+
+  # Return the validated department document.
+  return department
+
+def cursor_to_list(cursor):
+  """Converts a Cursor object to a list."""
+
+  list = []
+  for document in cursor:
+    list.append(document)
+  return list
+
+
+def insert_department(department,database):
+  """Inserts a department document into the collection."""
+
+  # Validate the department document.
+  validate_department(department)
+
+  # Check for existing departments with the same abbreviation, chair name, building, and office.
+  existing_departments = database.departments.find({
+    "$or": [
+      {"abbreviation": department["abbreviation"]},
+      {"chair_name": department["chair_name"]},
+      {"building": department["building"], "office": department["office"]},
+    ]
+  })
+
+  # Convert the Cursor object to a list.
+  existing_departments_list = cursor_to_list(existing_departments)
+
+  # Count the number of existing departments.
+  num_existing_departments = len(existing_departments_list)
+
+  if num_existing_departments > 0:
+    raise ValueError("There is already a department with the same abbreviation, chair name, building, or office.")
+
+  # Insert the department document into the collection.
+  database.departments.insert_one(department)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 
+import Department as Dp
+
 cluster = input("Input your connection link") or "mongodb+srv://rboixo:Carm3n1ta@atlascluster.wxir5tb.mongodb.net/?retryWrites=true&w=majority"
 
 
@@ -13,71 +15,6 @@ collection = db.Deparments
 
 #collection.insert_one({"name":"CECS"})
 
-def validate_department(department):
-  """Validates a department document."""
-
-  # Check if the required fields are present.
-  if not department.get("abbreviation"):
-    raise ValueError("The department abbreviation is required.")
-  if not department.get("chair_name"):
-    raise ValueError("The department chair name is required.")
-  if not department.get("building"):
-    raise ValueError("The department building is required.")
-  if not department.get("office"):
-    raise ValueError("The department office is required.")
-  if not department.get("description"):
-    raise ValueError("The department description is required.")
-
-  # Check the maximum lengths of the fields.
-  if len(department["name"]) > 50:
-    raise ValueError("The department name must be no longer than 50 characters.")
-  if len(department["abbreviation"]) > 6:
-    raise ValueError("The department abbreviation must be no longer than 6 characters.")
-  if len(department["chair_name"]) > 80:
-    raise ValueError("The department chair name must be no longer than 80 characters.")
-  if len(department["building"]) > 10:
-    raise ValueError("The department building must be no longer than 10 characters.")
-  if len(department["description"]) > 80:
-    raise ValueError("The department description must be no longer than 80 characters.")
-
-  # Return the validated department document.
-  return department
-
-def cursor_to_list(cursor):
-  """Converts a Cursor object to a list."""
-
-  list = []
-  for document in cursor:
-    list.append(document)
-  return list
-
-
-def insert_department(department):
-  """Inserts a department document into the collection."""
-
-  # Validate the department document.
-  validate_department(department)
-
-  # Check for existing departments with the same abbreviation, chair name, building, and office.
-  existing_departments = db.departments.find({
-    "$or": [
-      {"abbreviation": department["abbreviation"]},
-      {"chair_name": department["chair_name"]},
-      {"building": department["building"], "office": department["office"]},
-    ]
-  })
-
-  # Convert the Cursor object to a list.
-  existing_departments_list = cursor_to_list(existing_departments)
-
-  # Count the number of existing departments.
-  num_existing_departments = len(existing_departments_list)
-
-  if num_existing_departments > 0:
-    raise ValueError("There is already a department with the same abbreviation, chair name, building, or office.")
-
-  # Insert the department document into the collection.
-  db.departments.insert_one(department)
 
 
 department = {
@@ -90,7 +27,7 @@ department = {
 }
 
 try:
-  insert_department(department)
+  Dp.insert_department(department,db)
 except ValueError as e:
   print(e)
 
